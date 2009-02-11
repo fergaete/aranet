0850e70434840a703183b3b5213187626e694990
aranova <aranova@56d20148-af09-11dd-8830-69e4bb380a4a>
2009-02-11 15:03:43 +0000

    Rama 1.1, módulo Proyectos

--- apps/frontend/modules/project/templates/listJson.php
+++ apps/frontend/modules/project/templates/listJson.php
@@ -1,11 +1,24 @@
 {"recordsReturned":<?php echo $pager->getMaxPerPage() ?>,
-    "totalRecords":<?php echo $pager->getNbResults() ?>,
+    "totalRecords":<?php echo count($pager->getResults()) ?>,
     "startIndex":<?php echo $pager->getFirstIndice()-1 ?>,
     "records":[
 <?php $i = 0; foreach ($pager->getResults() as $project) : $i++ ?>
         {"id":"<?php echo $project->getId() ?>",
         "checkbox":"<input type='checkbox' name='select[]' value='<?php echo $project->getId() ?>' />",
         "actions":"<div class='objectActions'><ul><li><?php echo str_replace('"', "'", link_to(image_tag('icons/application_form.png', 'alt='.__("View")), '@project_show_by_id?id='.$project->getId())) ?></li><li><?php echo str_replace('"', "'", link_to(image_tag('icons/application_form_edit.png', 'alt='.__('Edit')), '@project_edit_by_id?id='.$project->getId())) ?></li><li><?php echo str_replace('"', "'", link_to(image_tag('icons/application_form_delete.png', 'alt='.__('Delete')), '@project_delete_by_id?id='.$project->getId())) ?></li></ul></div>",
+<<<<<<< .mine
+        "project_name":"<?php echo $project->getProjectName() ?>",
+        "project_title":"<?php echo $project->__toString() ?>",
+        "client":"<?php echo $project->getClient()->__toString() ?>",
+        "project_start_date":"<?php echo format_date($project->getProjectStartDate()) ?>",
+        "project_end_date":"<?php echo format_date($project->getProjectFinishDate()) ?>",
+        "project_number":"<?php echo $project->getFullNumber() ?>",
+        "project_status":"<?php echo $project->getProjectStatus() ?>",
+        "project_total_hours":"<?php //echo $project->getProjectStatus() ?>",
+        "project_total_incomes":"<?php //echo $project->getProjectStatus() ?>",
+        "project_total_expenses":"<?php //echo $project->getProjectStatus() ?>"}
+        <?php if ($i+$pager->getFirstIndice()-1 < count($pager->getResults())) : ?>,<?php endif ?>
+=======
         "project_name":"<?php echo $project->getProjectName() ?>",
         "project_title":"<?php echo $project->__toString() ?>",
         "client":"<?php echo $project->getClient()->__toString() ?>",
@@ -17,6 +30,7 @@
         "project_total_incomes":"<?php //echo $project->getProjectStatus() ?>",
         "project_total_expenses":"<?php //echo $project->getProjectStatus() ?>"}
         <?php if ($i+$pager->getFirstIndice()-1 < $pager->getLastIndice()) : ?>,<?php endif ?>
+>>>>>>> .r55
 <?php endforeach ?>
     ]
 }
--- lib/filter/ProjectFormFilter.class.php
+++ lib/filter/ProjectFormFilter.class.php
@@ -13,53 +13,54 @@ class ProjectFormFilter extends BaseProjectFormFilter
   public function configure()
   {
     parent::configure();
-    if (isset($this->object)) {
-      $name = $this->object->getName();
-    } else {
-      $name = '';//sfContext::getInstance()->sfI18N()->__('Name').'...';
-    }
-    $this->setWidgets(array(
-        'name'             => new sfWidgetFormInput(array(), array('onfocus' => 'this.value=""', 'onblur' => 'if (this.value=="") this.value="'.$name.'"')),
-      ));
-
-      $this->setValidators(array(
-        'name'          => new sfValidatorString(array('max_length' => 255)),
-      ));
-
-    $this->setWidgets(array(
-        'name'                  => new sfWidgetFormInput(),
-        'project_start_date'    => new yuiWidgetFormDate(),
-        'project_finish_date'   => new yuiWidgetFormDate(),
-        'project_status_id'     => 
-        new yuiWidgetFormPropelSelect(array('model' => 'ProjectStatus', 'add_empty' => true, 'multiple' => false), array('class' => 'yui_select')),
-        'project_category_id'     => 
-        new yuiWidgetFormPropelSelect(array('model' => 'ProjectCategory', 'add_empty' => true, 'multiple' => false), array('class' => 'yui_select'))
-    ));
-    //new sfWidgetFormPropelSelect(array('model' => 'ProjectCategory', 'add_empty' => true)),
-    // project_client_id
-    $this->widgetSchema['project_client_id'] = new yuiWidgetFormAutocomplete(array('delimChar' => array(','), 'formatResult' => '%1%.UniqueName', 'resultSchema' => '["ResultSet.Result","UniqueName"]', 'action' => '/client/autocomplete'), array('class' => 'tiny'));
     
-    $this->validatorSchema['project_client_id'] = new sfValidatorTags('name', new sfValidatorString(array('required' => false)));
-
-
-    $this->setValidators(array(
-        'name'                  => new sfValidatorPass(array('required' => false)),
-        'project_start_date'    => new sfValidatorDateTime(array('required' => false)),
-        'project_finish_date'   => new sfValidatorDateTime(array('required' => false)),
-        'project_status_id'     => new sfValidatorPropelChoice(array('model' => 'ProjectStatus', 'column' => 'id', 'required' => false)),
-        'project_category_id'     => new sfValidatorPropelChoice(array('model' => 'ProjectCatagory', 'column' => 'id', 'required' => false)),
+    $this->setWidgets(array(
+      'name' => new sfWidgetFormFilterInput(),
+      'project_start_date_from'    => new yuiWidgetFormDate(),
+      'project_start_date_to'    => new yuiWidgetFormDate(),
+      'project_finish_date_from'   => new yuiWidgetFormDate(),
+      'project_finish_date_to'   => new yuiWidgetFormDate(),
+      'project_status_id'     => new yuiWidgetFormPropelSelect(array('model' => 'ProjectStatus', 'add_empty' => true, 'multiple' => false), array('class' => 'yui_select')),
+      'project_category_id'   => new yuiWidgetFormPropelSelect(array('model' => 'ProjectCategory', 'add_empty' => true, 'multiple' => false), array('class' => 'yui_select')),
+      'project_client_id'     => new yuiWidgetFormAutocomplete(array('delimChar' => array(','), 'formatResult' => '%1%.UniqueName', 'resultSchema' => '["ResultSet.Result","UniqueName"]', 'action' => '/client/autocomplete'), array('class' => 'tiny')),
     ));
+    $this->setValidators(array(
+      'name' => new sfValidatorPass(array('required' => false)),
+      'project_start_date_from'    => new sfValidatorDateTime(array('required' => false)),
+      'project_start_date_to'    => new sfValidatorDateTime(array('required' => false)),
+      'project_finish_date_from'   => new sfValidatorDateTime(array('required' => false)),
+      'project_finish_date_to'   => new sfValidatorDateTime(array('required' => false)),
+      'project_status_id'     => new sfValidatorPropelChoice(array('model' => 'ProjectStatus', 'column' => 'id', 'required' => false)),
+      'project_category_id'   => new sfValidatorPropelChoice(array('model' => 'ProjectCatagory', 'column' => 'id', 'required' => false)),
+      'project_client_id'     => new sfValidatorTags('name', new sfValidatorString(array('required' => false))),
+    ));
+    
+
+    // project_client_id
+    if (isset($this->object) && $this->object->getClient()) {
+      $client_string = $this->object->getClient()->getFullName(false);
+      $client_id = $this->object->getClient()->getId();
+    } else {
+      $client_string = '';
+      $client_id = '';
+    }
+    $this->widgetSchema['project_client_id'] = new yuiWidgetFormAutocomplete(array('formatResult' => '%1%.FullName', 'resultSchema' => '["ResultSet.Result","FullName"]', 'action' => '/client/autocomplete', 'value' => array($client_string, $client_id)), array('class' => 'small'));
+    $this->validatorSchema['project_client_id'] = new sfValidatorTags('name', new sfValidatorString(array('required' => true)));
+
 
     $this->widgetSchema->setLabels(array(
-        'project_start_date'    => 'Start date',
-        'project_finish_date'   => 'End date',
-        'project_status_id'     => 'Status',
-        'project_category_id'   => 'Category',
-        'project_client_id'     => 'Client'
+        'project_start_date_from' => 'From (start)',
+        'project_start_date_to'   => 'To (start)',
+        'project_finish_date_from'=> 'From (end)',
+        'project_finish_date_to'  => 'To (End)',
+        'project_status_id'       => 'Status',
+        'project_category_id'     => 'Category',
+        'project_client_id'       => 'Client'
     ));
 
     $this->widgetSchema->setNameFormat('project_filters[%s]');
 
     $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
+
   }
 }
