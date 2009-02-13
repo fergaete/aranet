cab134d565a56cf1e0f902d255d02d932ad661aa
aranova <aranova@56d20148-af09-11dd-8830-69e4bb380a4a>
2009-02-13 04:49:47 +0000

    Rama 1.0, bug en la generacion de graficos

--- apps/frontend/modules/dashboard/templates/getGraphicSuccess.php
+++ apps/frontend/modules/dashboard/templates/getGraphicSuccess.php
@@ -1,1 +1,2 @@
-<img src="/dashboard/showGraphic?graphic_id=<?php echo $sf_params->get('graphic_id', -1) ?>" alt="Cash flow" style="width:100%"/>
+<img src="/dashboard/showGraphic?graphic_id=<?php echo $sf_params->get('graphic_id', -1) ?>" alt="<?php echo __('Graphic') 
+?>" style="width:100%"/>
